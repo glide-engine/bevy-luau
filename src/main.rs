48d3820ee8eed46316a4f132053a37220710078b
PizzaLvr49 <jhtodd2012@gmail.com>
2026-05-30 23:13:53 -0400

    basic example

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,32 @@ use bevy::prelude::*;
 use mluau::prelude::*;
 
 fn main() -> AppExit {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_lua)
+        .run()
+}
+
+fn setup_lua(mut commands: Commands) {
+    let lua = Lua::new();
+
+    let print_from_rust = lua
+        .create_function(|_, msg: String| {
+            info!("[Luau]: {}", msg);
+            Ok(())
+        })
+        .unwrap();
+
+    let globals = lua.globals();
+    globals.set("info", print_from_rust).unwrap();
+
+    lua.load(
+        r#"
+            info("Hello from Luau inside Bevy!")
+        "#,
+    )
+    .exec()
+    .unwrap();
+
+    commands.write_message(AppExit::Success);
 }
